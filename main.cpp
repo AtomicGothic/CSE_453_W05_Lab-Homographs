e07324a777babe732a0de6442b4c5d36afd48069
Adam Goff <adam@finetechinc.com>
2021-02-02 10:05:38 -0500

    Revert "Updated the main.cpp"
    This reverts commit 560aa2d959b1f39db2036748c0821fd5201816df.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
+#include <string>
+ 
+
+using namespace std;
+
+
+
+
+
+
+
+/**********************************************************************
+ * GetFile
+ * The function which gets the file name
+ ***********************************************************************/
+const string  GetFile()
+{
+	// get the fileName
+	string fName = "";
+	std::cout << "What is the filename? ";
+	std::cin >> fName;
+	return fName;
+}
+
+/**********************************************************************
+ * checkPath
+ * The function which checks Path Homograph
+ ***********************************************************************/
+bool checkPath()
+{
+	string fName1 = "";
+	string fName2 = "";
+
+	cout << "File name 1> ";
+	cin >> fName1;
+	cout << "File name 2> ";
+	cin >> fName2;
+
+	return true;
+}
+
+
+
+
 
 int main()
 {
+	if (checkPath() == true)
+	{
+		cout << "Homograph\n";
+	}
+	else
+	{
+		cout << "Nonhomograph\n";
+	}
+		
+	
     std::cout << "Hello World!\n";
 }
+
