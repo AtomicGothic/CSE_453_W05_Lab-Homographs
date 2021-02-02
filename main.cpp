5dd201ca324411a8a882ce96aa802e5da26f5a0a
J N <jerrald.nelson@gmail.com>
2021-02-02 09:25:56 -0700

    Finished up Path Homograph

--- main.cpp
+++ main.cpp
@@ -34,12 +34,33 @@ const string  GetFile()
 	return fName;
 }
 
+/**********************************************************************
+ * GetCurrentPath
+ * The function which gets current Path
+ ***********************************************************************/
 std::string get_current_dir() {
 	char buff[FILENAME_MAX]; //create string buffer to hold path
 	GetCurrentDir(buff, FILENAME_MAX);
 	string current_working_dir(buff);
 	return current_working_dir;
 }
+
+/**********************************************************************
+ * fullPath
+ * The function which checks full path
+ ***********************************************************************/
+string GetFullPathFromPartial(char* partialPath)
+{
+	char full[_MAX_PATH];
+	if (_fullpath(full, partialPath, _MAX_PATH) != NULL)
+		//printf("Full path is: %s\n", full);
+		return full;
+	else
+		//printf("Invalid path\n");
+		return "Invalid path\n";
+}
+
+
 
 /**********************************************************************
  * checkPath
@@ -49,15 +70,33 @@ bool checkPath()
 {
 	string fName1 = "";
 	string fName2 = "";
+	string fFullPath1 = "";
+	string fFullPath2 = "";
 
 	cout << "File name 1> ";
 	cin >> fName1;
 	cout << "File name 2> ";
 	cin >> fName2;
 
-	cout << get_current_dir() << "\\" << fName1 << "\n";
+	// Retrieving Full path of file 1
+	fFullPath1= get_current_dir() + "\\" + fName1;
 
-	return true;
+	//cout << get_current_dir() << "\\" << fName1 << "\n";
+
+	//Retrieving Full path of file 2
+	std::string str = fName2;
+	char* c = const_cast<char*>(str.c_str());
+	//std::cout << c;
+	fFullPath2 = GetFullPathFromPartial(c);
+
+	if (fFullPath1 == fFullPath2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 
@@ -66,6 +105,7 @@ bool checkPath()
 
 int main()
 {
+
 	if (checkPath() == true)
 	{
 		cout << "Homograph\n";
@@ -74,7 +114,6 @@ int main()
 	{
 		cout << "Non-homograph\n";
 	}
-		
 	
     std::cout << "Hello World!\n";
 }
